00fbb65804cae1aed879dd8187dd3b4027df7b1c
Alessandro Scolozzi <scolozzi.alessandro@gmail.com>
2021-03-24 11:00:35 +0100

    da for a while

---
+++ da-for-a-while/js/script.js
@@ -0,0 +1,12 @@
+var arrayNomi = ['Giorgio', 'Francesca', 'Attilio', 'Ernesto'];
+
+// for( var i = 1; i < 100; i++ ) {
+//     console.log( arrayNomi[i] );
+// }
+
+var i = 0;
+while( i < arrayNomi.length ) {
+    console.log( arrayNomi[i] );
+
+    i++;
+}
