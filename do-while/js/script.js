b3f78ade909c31910cf7c9499f58f79456d5a740
Alessandro Scolozzi <scolozzi.alessandro@gmail.com>
2021-03-24 12:10:36 +0100

    do while

---
+++ do-while/js/script.js
@@ -0,0 +1,11 @@
+// var stringaUtente = prompt("Dimmi il tuo nome");
+
+// while( stringaUtente.length == 0 ) {
+//     stringaUtente = prompt("Dimmi il tuo nome");
+// }
+
+do {
+    var stringaUtente = prompt("Dimmi il tuo nome");
+} while( stringaUtente.length == 0 );
+
+console.log(stringaUtente);
